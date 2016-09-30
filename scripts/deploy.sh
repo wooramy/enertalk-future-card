ac8583ca9bbd4cb446cdf09603e0db98d5258d06
Lee Sanghyeon <yongdamsh@gmail.com>
2016-10-01 01:47:11 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-node_modules/.bin/storybook-to-ghpages
+ORIGIN_URL=`git config --get remote.origin.url`
+node ./node_modules/.bin/build-storybook -o .out
+cd .out
+git init
+git config user.name "GH Pages Bot"
+git config user.email "yongdamsh@gmail.com"
+git add .
+git commit -m "Deploy Storybook to GitHub Pages"
+git push --force --quiet "$ORIGIN_URL" master:gh-pages
+cd ..
+rm -rf .out
+
+echo "Deployed successfully"
+exit 0
