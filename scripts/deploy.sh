613b3834ddd452f621d5d0a376ccdc833e536451
Lee Sanghyeon <yongdamsh@gmail.com>
2016-10-01 02:33:23 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ORIGIN_URL=`git config --get remote.origin.url`
+ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
 
 npm install
 npm run build-storybook
@@ -10,7 +11,7 @@ git config user.name "yongdamsh"
 git config user.email "yongdamsh@gmail.com"
 git add .
 git commit -m "Deploy Storybook to GitHub Pages"
-git push --force --quiet "$ORIGIN_URL" master:gh-pages
+git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" master:gh-pages
 cd ..
 rm -rf .out
 
